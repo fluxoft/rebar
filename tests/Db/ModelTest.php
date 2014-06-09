df1e0fb0381c77fc4803147a3ab4894b42954c0a
fluxoft <joe@fluxoft.com>
2014-06-09 17:35:56 -0400

    Db/Model test class placeholder

---
+++ tests/Db/ModelTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Fluxoft\Rebar\Db;
+
+class ModelTest extends \PHPUnit_Framework_TestCase {
+	protected function setup() {
+	}
+
+	protected function teardown() {
+
+	}
+
+	public function testTest() {
+		$this->assertNotEquals('foo', 'bar');
+	}
+}
