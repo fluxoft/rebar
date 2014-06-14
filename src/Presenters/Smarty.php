eaeb01cbd4468517c72221f4ce28e7b1e349a1c5
fluxoft <joe@fluxoft.com>
2014-06-14 19:40:13 -0400

    Presenters/Smarty
    Added some flexibility to how layouts and templates are specify

--- src/Presenters/Smarty.php
+++ src/Presenters/Smarty.php
@@ -3,25 +3,53 @@ namespace Fluxoft\Rebar\Presenters;
 
 use \Fluxoft\Rebar\Http\Response;
 
+/**
+ * Class Smarty
+ * @package Fluxoft\Rebar\Presenters
+ * @property string Layout
+ * @property string Template
+ */
 class Smarty implements PresenterInterface {
 	protected $smarty = null;
 	protected $templatePath;
 	protected $template;
 	protected $layout;
 
-	public function __construct(\Smarty $smarty, $templatePath, $template, $layout = 'default.tpl') {
+	public function __construct(\Smarty $smarty, $templatePath, $layout = 'default.tpl', $template = '') {
 		$this->smarty = $smarty;
 		$this->templatePath = $templatePath;
-		$this->template = $template;
 		$this->layout = $layout;
+		$this->template = $template;
 	}
 
 	public function Render(Response $response, array $data) {
-		$this->smarty->assign('templateFile', $this->templatePath.$this->template);
+		if (strlen($this->template)) {
+			$this->smarty->assign('templateFile', $this->templatePath.$this->template);
+		}
 		$this->smarty->assign($data);
 		$output = $this->smarty->fetch($this->templatePath.$this->layout);
 
 		$response->Body = $output;
 		$response->Send();
+	}
+	public function __set($var, $val) {
+		switch ($var) {
+			case 'Template':
+				$this->template = $val;
+				break;
+			case 'Layout':
+				$this->layout = $val;
+				break;
+		}
+	}
+	public function __get($var) {
+		switch ($var) {
+			case 'Template':
+				return $this->template;
+				break;
+			case 'Layout':
+				return $this->layout;
+				break;
+		}
 	}
 } 
