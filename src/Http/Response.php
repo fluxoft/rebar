b07df5b67b9514d313056bce9b5500013f5e54e7
Joe Hart <joehart@RoundSpheres-Macbook-Pro.local>
2015-07-27 13:09:29 -0400

    Http/Reponse tweak
    Allow for setting custom HTTP status messages, either by passing an
    array when setting the Status property:
    $reponse-Status = [200, ‘okie dokie’];
    
    or by explicitly setting the StatusMessage property:
    $response->Status = 200;
    $response->StatusMessage = “okie dokie”;

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -10,59 +10,60 @@ namespace Fluxoft\Rebar\Http;
  */
 class Response {
 	protected $status;
+	protected $statusMessage;
 	protected $body;
 	protected $headers;
 
 	protected $messages = [
 		//Informational 1xx
-		100 => '100 Continue',
-		101 => '101 Switching Protocols',
+		100 => 'Continue',
+		101 => 'Switching Protocols',
 		//Successful 2xx
-		200 => '200 OK',
-		201 => '201 Created',
-		202 => '202 Accepted',
-		203 => '203 Non-Authoritative Information',
-		204 => '204 No Content',
-		205 => '205 Reset Content',
-		206 => '206 Partial Content',
+		200 => 'OK',
+		201 => 'Created',
+		202 => 'Accepted',
+		203 => 'Non-Authoritative Information',
+		204 => 'No Content',
+		205 => 'Reset Content',
+		206 => 'Partial Content',
 		//Redirection 3xx
-		300 => '300 Multiple Choices',
-		301 => '301 Moved Permanently',
-		302 => '302 Found',
-		303 => '303 See Other',
-		304 => '304 Not Modified',
-		305 => '305 Use Proxy',
-		306 => '306 (Unused)',
-		307 => '307 Temporary Redirect',
+		300 => 'Multiple Choices',
+		301 => 'Moved Permanently',
+		302 => 'Found',
+		303 => 'See Other',
+		304 => 'Not Modified',
+		305 => 'Use Proxy',
+		306 => '(Unused)',
+		307 => 'Temporary Redirect',
 		//Client Error 4xx
-		400 => '400 Bad Request',
-		401 => '401 Unauthorized',
-		402 => '402 Payment Required',
-		403 => '403 Forbidden',
-		404 => '404 Not Found',
-		405 => '405 Method Not Allowed',
-		406 => '406 Not Acceptable',
-		407 => '407 Proxy Authentication Required',
-		408 => '408 Request Timeout',
-		409 => '409 Conflict',
-		410 => '410 Gone',
-		411 => '411 Length Required',
-		412 => '412 Precondition Failed',
-		413 => '413 Request Entity Too Large',
-		414 => '414 Request-URI Too Long',
-		415 => '415 Unsupported Media Type',
-		416 => '416 Requested Range Not Satisfiable',
-		417 => '417 Expectation Failed',
-		418 => '418 I\'m a teapot',
-		422 => '422 Unprocessable Entity',
-		423 => '423 Locked',
+		400 => 'Bad Request',
+		401 => 'Unauthorized',
+		402 => 'Payment Required',
+		403 => 'Forbidden',
+		404 => 'Not Found',
+		405 => 'Method Not Allowed',
+		406 => 'Not Acceptable',
+		407 => 'Proxy Authentication Required',
+		408 => 'Request Timeout',
+		409 => 'Conflict',
+		410 => 'Gone',
+		411 => 'Length Required',
+		412 => 'Precondition Failed',
+		413 => 'Request Entity Too Large',
+		414 => 'Request-URI Too Long',
+		415 => 'Unsupported Media Type',
+		416 => 'Requested Range Not Satisfiable',
+		417 => 'Expectation Failed',
+		418 => 'I\'m a teapot',
+		422 => 'Unprocessable Entity',
+		423 => 'Locked',
 		//Server Error 5xx
-		500 => '500 Internal Server Error',
-		501 => '501 Not Implemented',
-		502 => '502 Bad Gateway',
-		503 => '503 Service Unavailable',
-		504 => '504 Gateway Timeout',
-		505 => '505 HTTP Version Not Supported'
+		500 => 'Internal Server Error',
+		501 => 'Not Implemented',
+		502 => 'Bad Gateway',
+		503 => 'Service Unavailable',
+		504 => 'Gateway Timeout',
+		505 => 'HTTP Version Not Supported'
 	];
 
 	public function __construct(
@@ -107,12 +108,25 @@ class Response {
 	protected function getStatus() {
 		return $this->status;
 	}
-	protected function setStatus($value) {
-		if (isset($this->messages[$value])) {
-			$this->status = $value;
+	protected function setStatus($status) {
+		if (is_array($status)) {
+			$this->status        = $status[0];
+			$this->statusMessage = $status[1];
 		} else {
-			throw new Exceptions\InvalidStatusException(sprintf('Status %s is not supported.', $value));
+			$code = $status;
+			if (isset($this->messages[$code])) {
+				$this->status        = $code;
+				$this->statusMessage = $this->messages[$code];
+			} else {
+				throw new Exceptions\InvalidStatusException(sprintf('Status %s is not supported.', $value));
+			}
 		}
+	}
+	protected function getStatusMessage() {
+		return $this->statusMessage;
+	}
+	protected function setStatusMessage($message) {
+		$this->statusMessage = $message;
 	}
 	protected function getBody() {
 		return $this->body;
