d3761b8308a3725084a68313c36057554937c104
joe-hart <joe.hart@gmail.com>
2015-11-24 20:09:11 -0500

    argh...

--- src/Rest/Controller.php
+++ src/Rest/Controller.php
@@ -2,148 +2,130 @@
 
 namespace Fluxoft\Rebar\Rest;
 
-use Fluxoft\Rebar\Container;
-use Fluxoft\Rebar\Controller as BaseController;
-use Fluxoft\Rebar\Exceptions\RouterException;
-use Fluxoft\Rebar\Presenters\Json;
+use Doctrine\DBAL\DBALException;
+use Fluxoft\Rebar\Db\Mapper;
+use Psr\Log\LoggerInterface;
 
-abstract class Controller extends BaseController {
-	protected function run(
-		RepositoryInterface $repository,
-		array $params,
-		array $config = null
-	) {
-		$allowed = (isset($config['allowed']) ? $config['allowed'] : ['GET']);
+class Repository implements RepositoryInterface {
+	protected $mapper;
+	protected $logger;
 
-		$method   = $this->request->Method;
-		$getVars  = $this->request->Get();
-		$postVars = $this->request->Post();
-		$putVars  = $this->request->Put();
-		$body     = $this->request->Body;
+	public function __construct(Mapper $mapper, LoggerInterface $logger = null) {
+		$this->mapper = $mapper;
+		$this->logger = $logger;
+	}
 
-		// Force Json presenter for this type of controller (so all replies are in JSON format)
-		// and set its Callback property from the value in $getVars['callback'], then unset that
-		// value from the array if it exists.
-		$this->presenterClass = 'Json';
-		$this->presenter      = new Json();
-		$this->presenter->SetCallback($this->request->Get('callback', ''));
-		unset($getVars['callback']);
-
-		if (!in_array($method, $allowed)) {
-			$response = [403, ['error' => "The {$method} method is not permitted here."]];
-		} else {
-			switch ($method) {
-				case 'GET':
-					/**
-					 * GET /{item}                 <- retrieve a set
-					 * GET /{item}?page={page}     <- retrieve page {page} of results
-					 * GET /{item}/{id}            <- retrieve {item} with id {id}
-					 * GET /{item}/{id}/{children} <- retrieve the children of {item} with id {id}
-					 *     ** the above only works on Mappers which have a Get{children} method accepting {id} as an argument
-					 */
-					$get      = $getVars;
-					$page     = (isset($get['page']) && is_numeric($get['page'])) ? $get['page'] : 1;
-					$pageSize = 0;
-					if (isset($config['pageSize']) && is_numeric($config['pageSize'])) {
-						$pageSize = $config['pageSize'];
-					}
-					if (isset($get['pageSize']) && is_numeric($get['pageSize'])) {
-						$pageSize = $get['pageSize'];
-					}
-
-					unset($get['page']);
-					unset($get['pageSize']);
-
-					if (!isset($params[0])) {
-						$params = [];
-					} elseif (!isset($params[1])) {
-						$params = [$params[0]];
-					} elseif (!isset($params[2])) {
-						$params = [$params[0], $params[1]];
-					}
-
-					switch (count($params)) {
-						case 0:
-							$response = $repository->GetSet($get, $page, $pageSize);
-							break;
-						case 1:
-							// assume the first params value is the ID of an item
-							$response = $repository->GetOne($params[0]);
-							break;
-						case 2:
-							$response = $repository->GetSubset($params[0], $params[1], $page, $pageSize);
-							break;
-						default:
-							$response = [
-								400,
-								['error' => 'Too many parameters in URL.']
-							];
-							break;
-					}
-					break;
-
-				case 'POST':
-					/**
-					 * POST /{items} <- create an {item} using POST data
-					 */
-					if (isset($postVars['model'])) {
-						$model = json_decode($postVars['model'], true);
-					} elseif (!empty($postVars)) {
-						$model = $postVars;
-					} elseif (strlen($body) > 0) {
-						$model = json_decode($body, true);
-					} else {
-						$model = [];
-					}
-
-					$response = $repository->Post($model);
-					break;
-
-				case 'PUT':
-					/**
-					 * PUT /{item}/{id} <- UPDATE an {item} with ID {id} using POST/PUT params
-					 */
-					if (empty($params)) {
-						$response = [422, ['error' => 'You must specify an ID in order to update.']];
-					} else {
-						if (isset($putVars['model'])) {
-							$model = json_decode($putVars['model'], true);
-						} elseif (!empty($putVars)) {
-							$model = $putVars;
-						} elseif (strlen($body) > 0) {
-							$model = json_decode($body, true);
-						} else {
-							$model = [];
-						}
-						$response = $repository->Put($params[0], $model);
-					}
-					break;
-
-				case 'DELETE':
-					if (empty($params)) {
-						// cannot delete if we don't have an id
-						$response = [422, ['error' => 'ID is required for DELETE operation.']];
-					} else {
-						$response = $repository->Delete($params[0]);
-					}
-					break;
-
-				default:
-					$response = [405, ['error' => 'Unsupported method.']];
-			}
+	public function GetSet(array $filter = [], $page = 1, $pageSize = 0) {
+		// if the params array was empty, return a set
+		$filterKeys   = [];
+		$filterValues = [];
+		foreach ($filter as $key => $value) {
+			$filterKeys[]          = '{'.$key.'} = :'.$key;
+			$filterValues[":$key"] = $value;
 		}
 
-		if ($response === false) {
-			$this->response->Status = 404;
-			$this->set('error', 'Not found');
-		} elseif (!is_array($response)) {
-			$this->response->Status = 500;
-			$this->set('error', 'Bad response from repository');
+		$whereClause = implode(' AND ', $filterKeys);
+
+		$set = $this->mapper->GetSetWhere($whereClause, $filterValues, $page, $pageSize);
+
+		return [
+			200,
+			$set
+		];
+	}
+
+	public function GetOne($id) {
+		$item = $this->mapper->GetOneById($id);
+		if ($item === false) {
+			$response = [
+				404,
+				[
+					'error' => 'The requested item could not be found.'
+				]
+			];
 		} else {
-			$this->response->Status = $response[0];
-			foreach ($response[1] as $key => $value) {
-				$this->set($key, $value);
+			$response = [
+				200,
+				$item
+			];
+		}
+		return $response;
+	}
+
+	public function GetSubset($id, $subsetName, $page = 1, $pageSize = 0) {
+		$method = 'Get'.ucwords($subsetName);
+		if (!method_exists($this->mapper, $method)) {
+			$response = [
+				404,
+				[
+					'error' => sprintf('"%s" not found.', $subsetName)
+				]
+			];
+		} else {
+			$subset   = $this->mapper->$method($id, $page, $pageSize);
+			$response = [
+				200,
+				$subset
+			];
+		}
+		return $response;
+	}
+
+	public function Post(array $post = []) {
+		try {
+			$new = $this->mapper->GetNew();
+			foreach ($post as $key => $value) {
+				$new->$key = $value;
 			}
+			$this->mapper->Save($new);
+			$response = [201, $new];
+		} catch (\InvalidArgumentException $e) {
+			$response = [422, ['error' => $e->getMessage()]];
+		} catch (DBALException $e) {
+			$this->log('error', $e->getMessage());
+			$response = [
+				500,
+				['error' => 'Database error. Please try again later.']
+			];
+		} catch (\Exception $e) {
+			$response = [500, ['error' => $e->getMessage()]];
+		}
+		return $response;
+	}
+
+	public function Put($id, array $model) {
+		/** @var \Fluxoft\Rebar\Db\Model $update */
+		$update = $this->mapper->GetOneById($id);
+		if ($update === false) {
+			$response = [404, ['error' => 'The object to be updated was not found.']];
+		} else {
+			$errors = [];
+			foreach ($model as $key => $value) {
+				try {
+					$update->$key = $value;
+				} catch (\InvalidArgumentException $e) {
+					$errors[] = $e->getMessage();
+				}
+			}
+			if (empty($errors)) {
+				$this->mapper->Save($update);
+				$response = [200, $update];
+			} else {
+				$response = [422, ['errors' => $errors]];
+			}
+		}
+		return $response;
+	}
+
+	public function Delete($id) {
+		$this->mapper->DeleteOneById($id);
+		$response = [204, ['success' => 'The item was deleted.']];
+		return $response;
+	}
+
+	protected function log($type, $message) {
+		if (isset($this->logger)) {
+			$this->logger->$type($message);
 		}
 	}
 }
