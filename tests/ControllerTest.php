987bd8ceb555f14a7e431e585fd5243216a602b0
joe-hart <joe.hart@gmail.com>
2016-05-18 21:21:31 -0400

    removed method
    Forgot to remove this from the test.

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -37,10 +37,6 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
 	public function testDisplay() {
 		$this->controller->Display();
 	}
-
-	public function testDenyAccess() {
-		$this->controller->DenyAccess('denied');
-	}
 }
 
 class DummyController extends Controller {
