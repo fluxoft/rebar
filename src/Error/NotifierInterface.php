feb71aa1f969bed61c260e5fc01c07030c5f1bab
joe-hart <joe.hart@gmail.com>
2017-01-31 12:46:05 -0500

    php 7 error handling
    Notifier classes will now receive a `\Throwable` class instead of
    `\Exception` in order to allow for handling new `\Error` class in PHP7

--- src/Error/NotifierInterface.php
+++ src/Error/NotifierInterface.php
@@ -7,8 +7,8 @@ interface NotifierInterface {
 	 * Should be overridden in a Notifier class to accept an unhandled exception and do
 	 * something with it. These classes should be very careful to handle all possible
 	 * exceptions of their own in a graceful way so as not to cause a
-	 * @param \Exception $e
+	 * @param \Throwable $t
 	 * @return mixed
 	 */
-	public function Notify(\Exception $e);
+	public function Notify(\Throwable $t);
 }
