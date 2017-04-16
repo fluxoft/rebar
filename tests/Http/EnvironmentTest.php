7b87995ce090d6306ae264e3c44d63b9fcac3a4d
joe-hart <joe.hart@gmail.com>
2017-04-15 22:54:43 -0400

    Environment mock constructor test

--- tests/Http/EnvironmentTest.php
+++ tests/Http/EnvironmentTest.php
@@ -4,6 +4,11 @@ namespace Fluxoft\Rebar\Http;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class EnvironmentTest
+ * @package Fluxoft\Rebar\Http
+ * @coversDefaultClass \Fluxoft\Rebar\Http\Environment
+ */
 class EnvironmentTest extends TestCase {
 	protected function setup() {}
 
@@ -11,10 +16,34 @@ class EnvironmentTest extends TestCase {
 
 	/**
 	 * @dataProvider settingsProvider
+	 * @param $settings
 	 */
 	public function testDefaultEnvironmentSettings(array $settings) {
 		/** @var \Fluxoft\Rebar\Http\Environment $env */
 		$env = Environment::GetMock($settings);
+
+		/*
+		 * If no settings are given, check the defaults from the Environment class are set.
+		 */
+		if (empty($settings)) {
+			$defaults = [
+				'method' => 'GET',
+				'SCRIPT_NAME' => '',
+				'pathInfo' => '',
+				'QUERY_STRING' => '',
+				'SERVER_NAME' => 'localhost',
+				'SERVER_PORT' => 80,
+				'ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+				'ACCEPT_LANGUAGE' => 'en-US,en;q=0.8',
+				'ACCEPT_CHARSET' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+				'USER_AGENT' => 'Rebar',
+				'REMOTE_ADDR' => '127.0.0.1',
+				'rebar.protocol' => 'http',
+				'rebar.input' => ''
+			];
+
+			$settings = $defaults;
+		}
 
 		foreach ($settings as $key => $value) {
 			$this->assertEquals($env[$key], $settings[$key]);
