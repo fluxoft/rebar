be3373e69ea105e22672818a7eaa60883ca37c63
joe-hart <joe.hart@gmail.com>
2017-04-20 17:38:15 -0400

    CookieTest and SessionTest 100%

--- tests/Http/CookiesTest.php
+++ tests/Http/CookiesTest.php
@@ -5,15 +5,88 @@ namespace Fluxoft\Rebar\Http;
 use PHPUnit\Framework\TestCase;
 
 class CookiesTest extends TestCase {
-	protected function setup() {
+	/**
+	 * @param array $settings
+	 * @param array $cookieSet
+	 * @param $cookieReturn
+	 * @dataProvider cookiesProvider
+	 */
+	public function test($settings = [], $cookieSet = [], $cookieReturn) {
+		$cookies = new MockableCookies($settings);
 
+		$cookies->SetCookies($cookieSet);
+		$cookies->SetCookieReturn($cookieReturn);
+
+		// test getting entire array with no parameters to Get
+		$this->assertEquals($cookieSet, $cookies->Get());
+
+		// test getting default value for non-existent key
+		$this->assertEquals('default', $cookies->Get('nonExistent', 'default'));
+
+		foreach ($cookies as $key => $value) {
+			$this->assertEquals($value, $cookies->Get($key));
+		}
+
+		// test set and delete
+		$cookies->Set('new_cookie', 'new_value');
+		if ($cookieReturn) {
+			$this->assertEquals('new_value', $cookies->Get('new_cookie'));
+		} else {
+			$this->assertEquals(null, $cookies->Get('new_cookie'));
+		}
+
+		$cookies->Delete('new_cookie');
+		$this->assertEquals(null, $cookies->Get('new_test_key'));
+	}
+	public function cookiesProvider() {
+		return [
+			'blank' => [
+				'settings' => [],
+				'cookieSet' => [],
+				'cookiesReturn' => true
+			],
+			'failedSet' => [
+				'settings' => [],
+				'cookieSet' => [],
+				'cookiesReturn' => false
+			]
+		];
+	}
+}
+
+// @codingStandardsIgnoreStart
+class MockableCookies extends Cookies {
+	// @codingStandardsIgnoreEnd
+
+	private $cookieReturn = false;
+	protected function setCookie(
+		$key,
+		$value,
+		$expires,
+		$path,
+		$domain,
+		$secure,
+		$httponly
+	) {
+		unset($key);
+		unset($value);
+		unset($expires);
+		unset($path);
+		unset($domain);
+		unset($secure);
+		unset($httponly);
+
+		return $this->cookieReturn;
+	}
+	public function SetCookieReturn($cookieReturn) {
+		$this->cookieReturn = $cookieReturn;
 	}
 
-	protected function teardown() {
-
+	private $cookieSet = [];
+	protected function superGlobalCookies() {
+		return $this->cookieSet;
 	}
-
-	public function testFooNotEqualBar() {
-		$this->assertNotEquals('foo', 'bar');
+	public function SetCookies($cookieSet) {
+		$this->cookieSet = $cookieSet;
 	}
 }
--- tests/Http/SessionTest.php
+++ tests/Http/SessionTest.php
@@ -5,15 +5,61 @@ namespace Fluxoft\Rebar\Http;
 use PHPUnit\Framework\TestCase;
 
 class SessionTest extends TestCase {
-	protected function setup() {
+	/**
+	 * @param array $sessionSet
+	 * @dataProvider sessionProvider
+	 */
+	public function test($sessionSet = []) {
+		$session = new MockableSession();
 
+		$session->SetSessionParams($sessionSet);
+
+		// test getting entire array with no parameters to Get
+		$this->assertEquals($sessionSet, $session->Get());
+
+		// test getting default value for non-existent key
+		$this->assertEquals('default', $session->Get('nonExistent', 'default'));
+
+		foreach ($session as $key => $value) {
+			$this->assertEquals($value, $session->Get($key));
+		}
+
+		// test set and delete
+		$session->Set('new_test_key', 'new_test_value');
+		$this->assertEquals('new_test_value', $session->Get('new_test_key'));
+
+		$session->Delete('new_test_key');
+		$this->assertEquals(null, $session->Get('new_test_key'));
+	}
+	public function sessionProvider() {
+		return [
+			'blank' => [
+				'sessionSet' => []
+			]
+		];
+	}
+}
+
+// @codingStandardsIgnoreStart
+class MockableSession extends Session {
+	// @codingStandardsIgnoreEnd
+
+	private $sessionSet = [];
+	protected function superGlobalSession() {
+		return $this->sessionSet;
+	}
+	public function SetSessionParams($sessionSet) {
+		$this->sessionSet = $sessionSet;
 	}
 
-	protected function teardown() {
-
+	protected function startSession() {
+		return true;
 	}
 
-	public function testFooNotEqualBar() {
-		$this->assertNotEquals('foo', 'bar');
+	protected function setSession($key, $value) {
+		return true;
+	}
+	protected function unsetSession($key) {
+		return true;
 	}
 }
