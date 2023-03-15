de3f5b45c1508c7a5c93975e82fa138334e24edb
Joe Hart <joe.hart@gmail.com>
2023-03-15 00:47:59 -0400

    Filter setters
    Added setters for Operator and Value to insure that only valid operators can be set and that values make sense for IN and BETWEEN operators

--- src/Db/Filter.php
+++ src/Db/Filter.php
@@ -6,6 +6,7 @@ use Fluxoft\Rebar\_Traits\ArrayAccessibleProperties;
 use Fluxoft\Rebar\_Traits\GettableProperties;
 use Fluxoft\Rebar\_Traits\IterableProperties;
 use Fluxoft\Rebar\_Traits\SettableProperties;
+use Fluxoft\Rebar\Db\Exceptions\InvalidFilterException;
 
 /**
  * @property string Property
@@ -24,8 +25,8 @@ class Filter implements \ArrayAccess, \Iterator {
 	 */
 	protected $properties    = [
 		'Property' => '',
-		'Operator' => '=',
-		'Value' => ''
+		'Operator' => null,
+		'Value' => null
 	];
 	protected $modProperties = [];
 
@@ -34,8 +35,55 @@ class Filter implements \ArrayAccess, \Iterator {
 		string $operator,
 		mixed  $value
 	) {
-		$this->properties['Property'] = $property;
-		$this->properties['Operator'] = $operator;
-		$this->properties['Value']    = $value;
+		$this->Property = $property;
+		$this->Operator = $operator;
+		$this->Value    = $value;
+	}
+
+	/**
+	 * @param string $operator
+	 * @return void
+	 * @throws InvalidFilterException
+	 */
+	protected function setOperator(string $operator):void {
+		$this->properties['operator'] = match (strtoupper($operator)) {
+			'=', '<', '>', '<=', '>=', '<>', '!=', 'IN', 'LIKE', 'BETWEEN' => strtoupper($operator),
+			default => throw new InvalidFilterException(
+				'Invalid operator given. Must be one of the following: =, <, >, <=, >=, <>, !=, IN, LIKE, BETWEEN'
+			),
+		};
+	}
+
+	/**
+	 * @param mixed $value
+	 * @return void
+	 * @throws InvalidFilterException
+	 */
+	protected function setValue(mixed $value): void {
+		if ($this->Operator === 'IN') {
+			// for IN operator, value must be an array
+			if (is_array($value)) {
+				$this->properties['Value'] = $value;
+			} else {
+				throw new InvalidFilterException('Filter operator set to IN. Value must be an array of values.');
+			}
+			return;
+		}
+		if ($this->Operator === 'BETWEEN') {
+			// for BETWEEN, value must be an array with 2 values (low/high)
+			if (is_array($value) && count($value) === 2) {
+				$this->properties['Value'] = $value;
+			} else {
+				throw new InvalidFilterException(
+					'Filter operator set to BETWEEN. Value must be an array with two elements, e.g. [$low, $high].'
+				);
+			}
+			return;
+		}
+		// for other operators, value cannot be an array
+		if (is_array($value)) {
+			throw new InvalidFilterException('Cannot set Value to an array for the given Operator');
+		}
+		$this->properties['Value'] = $value;
 	}
 }
